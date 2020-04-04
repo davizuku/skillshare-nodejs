972b4eae3fff94f7ea0d55b9cf96e44b727cc6f9
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-04 12:36:02 +0200

    Obtain the request trimmed path

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 
 const http = require('http');
+const url = require('url');
 
 var server = http.createServer(function (req, res) {
-    res.end("hello world\n");
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    res.end("Hello World\n");
+
+    console.log('Request received on path: ' + trimmedPath);
+
 });
 
 server.listen(3000, function() {
