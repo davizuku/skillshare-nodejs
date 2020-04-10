4af5b37d1e3454abef62cb20cc542dbcd0ce7ab3
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-10 17:06:24 +0200

    Add compress/decompress functions to logs module

--- api/lib/logs.js
+++ api/lib/logs.js
@@ -48,4 +48,59 @@ lib.list = function(includeCompressedLogs, callback) {
     });
 };
 
+lib.compress = function(logId, newFileId, callback) {
+    var sourceFile = logId + '.log';
+    var destFile = newFileId + '.gz.b64';
+    fs.readFile(lib.baseDir + sourceFile, 'utf8', function (err, inputString) {
+        if (!err && inputString) {
+            zlib.gzip(inputString, function (err, buffer) {
+                if (!err && buffer) {
+                    fs.open(lib.baseDir + destFile, 'wx', function (err, fileDescriptor) {
+                        if (!err && fileDescriptor) {
+                            fs.writeFile(fileDescriptor, buffer.toString('base64'), function (err) {
+                                if (!err) {
+                                    fs.close(fileDescriptor, function (err) {
+                                        if (!err) {
+                                            callback(false);
+                                        } else {
+                                            callback(err);
+                                        }
+                                    });
+                                } else {
+                                    callback(err);
+                                }
+                            });
+                        } else {
+                            callback(err);
+                        }
+                    });
+                } else {
+                    callback(err);
+                }
+            });
+        } else {
+            callback(err);
+        }
+    });
+};
+
+lib.decompress = function (fileId, callback) {
+    var fileName = fileId + '.gz.b64';
+    fs.readFile(lib.baseDir + fileName, 'utf8', function (err, str) {
+        if (!err && str) {
+            var inputBuffer = Buffer.from(str, 'base64');
+            zlib.unzip(inputBuffer, function (err, outputBuffer) {
+                if (!err && outputBuffer) {
+                    var str = outputBuffer.toString();
+                    callback(false, str);
+                } else {
+                    callback(err);
+                }
+            });
+        } else {
+            callback(err);
+        }
+    });
+};
+
 module.exports = lib;
