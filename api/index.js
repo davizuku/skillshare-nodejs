6684a9e33a9c95435295a5c83ac86de3ce8b09c6
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-10 12:56:12 +0200

    Move server logic to a different file

--- api/index.js
+++ api/index.js
@@ -1,72 +1,12 @@
 
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
 
-var httpServer = http.createServer(function (req, res) {
-    unifiedServer(req, res);
-});
-httpServer.listen(config.httpPort, function() {
-    console.log("The server is listening on port " + config.httpPort + " in " + config.envName + " mode");
-});
-var httpsServerOptions = {
-    'key': fs.readFileSync(__dirname + '/https/key.pem'),
-    'cert': fs.readFileSync(__dirname + '/https/cert.pem'),
-};
-var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
-    unifiedServer(req, res);
-});
-httpsServer.listen(config.httpsPort, function() {
-    console.log("The server is listening on port " + config.httpsPort + " in " + config.envName + " mode");
-});
+var app = {};
 
-var unifiedServer = function (req, res) {
-    var parsedUrl = url.parse(req.url, true);
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    var method = req.method.toLowerCase();
-    var queryStringObject = parsedUrl.query; // This is available due to second parameter in url.parse
-    var headers = req.headers
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function (data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function () {
-        buffer += decoder.end();
-        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ?
-            router[trimmedPath] :
-            handlers.notFound;
-        var data = {
-            'trimmedPath': trimmedPath,
-            'queryStringObject': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer),
-        };
-        chosenHandler(data, function (statusCode, payload) {
-            statusCode = typeof (statusCode) !== 'undefined' ? statusCode : 200;
-            payload = typeof (payload) === 'object' ? payload : {};
-            var payloadString = JSON.stringify(payload);
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-            console.log({
-                'request': data,
-                'response': payload,
-            });
-        });
-    });
+app.init = function() {
+    server.init();
 };
 
-var router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks,
-};
+app.init();
+
+module.exports = app;
---
+++ api/lib/server.js
@@ -0,0 +1,81 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
+const handlers = require('./handlers');
+const helpers = require('./helpers');
+const path = require('path');
+
+var server = {};
+
+server.httpServer = http.createServer(function (req, res) {
+    server.unifiedServer(req, res);
+});
+
+server.httpsServerOptions = {
+    'key': fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
+    'cert': fs.readFileSync(path.join(__dirname, '/../https/cert.pem')),
+};
+server.httpsServer = https.createServer(server.httpsServerOptions, function (req, res) {
+    server.unifiedServer(req, res);
+});
+
+server.unifiedServer = function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    var method = req.method.toLowerCase();
+    var queryStringObject = parsedUrl.query; // This is available due to second parameter in url.parse
+    var headers = req.headers
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function () {
+        buffer += decoder.end();
+        var chosenHandler = typeof (server.router[trimmedPath]) !== 'undefined' ?
+            server.router[trimmedPath] :
+            handlers.notFound;
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(buffer),
+        };
+        chosenHandler(data, function (statusCode, payload) {
+            statusCode = typeof (statusCode) !== 'undefined' ? statusCode : 200;
+            payload = typeof (payload) === 'object' ? payload : {};
+            var payloadString = JSON.stringify(payload);
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log({
+                'request': data,
+                'response': payload,
+            });
+        });
+    });
+};
+
+server.router = {
+    'ping': handlers.ping,
+    'users': handlers.users,
+    'tokens': handlers.tokens,
+    'checks': handlers.checks,
+};
+
+server.init = function() {
+    server.httpServer.listen(config.httpPort, function () {
+        console.log("The server is listening on port " + config.httpPort + " in " + config.envName + " mode");
+    });
+    server.httpsServer.listen(config.httpsPort, function () {
+        console.log("The server is listening on port " + config.httpsPort + " in " + config.envName + " mode");
+    });
+
+}
+
+module.exports = server;
