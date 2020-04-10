d99e4713bf44a54b9d57603d3a049fd286ca3658
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-10 12:18:42 +0200

    Add method to interact with twilio API

--- api/lib/helpers.js
+++ api/lib/helpers.js
@@ -1,6 +1,8 @@
 
 const crypto = require('crypto');
 const config = require('../config');
+const https = require('https');
+const querystring = require('querystring');
 
 var helpers = {};
 
@@ -34,5 +36,48 @@ helpers.createRandomString = function(strLength) {
         return false;
     }
 }
+
+helpers.sendTwilioSms = function (phone, msg, callback) {
+    phone = typeof(phone) == 'string' && phone.trim().length <= 10 ?
+        phone.trim() :
+        false;
+    msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ?
+        msg.trim() :
+        false;
+    if (phone && msg) {
+        var payload = {
+            'From': config.twilio.fromPhone,
+            'To': '+34' + phone,
+            'Body': msg,
+        };
+        var stringPayload = querystring.stringify(payload);
+        var requestDetails = {
+            'protocol': 'https:',
+            'hostname': 'api.twilio.com',
+            'method': 'POST',
+            'path': '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
+            'auth': config.twilio.accountSid + ':' + config.twilio.authToken,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload),
+            }
+        };
+        var req = https.request(requestDetails, function(res) {
+            var status = res.statusCode;
+            if (status == 200 || status == 201) {
+                callback(false);
+            } else {
+                callback('Status code returned was ' + statusCode)
+            }
+        });
+        req.on('error', function(e) {
+            callback(e);
+        });
+        req.write(stringPayload);
+        req.end();
+    } else {
+        callback('Given parameters were missing or invalid');
+    }
+};
 
 module.exports = helpers;
