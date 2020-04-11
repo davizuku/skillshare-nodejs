9e1f295b2d4600c2825818735a76188004881b37
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-11 12:18:25 +0200

    Add getTemplate function

--- gui/lib/helpers.js
+++ gui/lib/helpers.js
@@ -1,4 +1,7 @@
 
+const fs = require('fs');
+const path = require('path');
+
 var lib = {};
 
 // Node will throw an error if JSON parsing fails. This function avoids this.
@@ -10,8 +13,22 @@ lib.parseJsonToObject = function (str) {
     }
 };
 
-lib.getTemplate = function (name, callback) {
-
+lib.getTemplate = function (templateName, callback) {
+    templateName = typeof(templateName) == 'string' && templateName.length > 0 ?
+        templateName :
+        false;
+    if (templateName) {
+        var templatesDir = path.join(__dirname + '/../templates/');
+        fs.readFile(templatesDir + templateName + '.html', 'utf8', function (err, str) {
+            if (!err && str && str.length > 0) {
+                callback(false, str);
+            } else {
+                callback('No template could be found');
+            }
+        });
+    } else {
+        callback('A valid template name was not specified');
+    }
 };
 
 module.exports = lib;
