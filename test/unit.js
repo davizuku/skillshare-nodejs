05cef9b44e71e6c149eadbde2960154843bb59b9
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-04-13 16:33:54 +0200

    Add unit tests for logs and other examples

--- test/unit.js
+++ test/unit.js
@@ -1,4 +1,6 @@
 const apiHelpers = require('./../api/lib/helpers');
+const logs = require('./../api/lib/logs');
+const exampleDebuggingProblem = require('./../api/lib/exampleDebuggingProblem');
 const assert = require('assert');
 
 var unit = {};
@@ -19,6 +21,31 @@ unit['helpers.getANumber should return 2'] = function (done) {
     var val = apiHelpers.getANumber();
     assert.equal(val, 2);
     done();
+};
+
+unit['logs.list should callback a false error and an array of log names'] = function (done) {
+    logs.list(true, function (err, logFileNames) {
+        assert.equal(err, false);
+        assert.ok(logFileNames instanceof Array);
+        assert.ok(logFileNames.length > 1);
+        done();
+    });
+};
+
+unit['logs.truncate should not throw if the log id does not exist. It should callback an error instead'] = function (done) {
+    assert.doesNotThrow(function () {
+        logs.truncate('I do not exist', function (err) {
+            assert.ok(err);
+            done();
+        });
+    }, TypeError);
+};
+
+unit['exampleDebuggingProblem.init should not throw when called'] = function (done) {
+    assert.doesNotThrow(function () {
+        exampleDebuggingProblem.init();
+        done();
+    }, TypeError);
 };
 
 module.exports = unit;
